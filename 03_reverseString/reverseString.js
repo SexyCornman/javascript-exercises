8a94c2b4757fca279254a02bb02a67c0904e3081
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-09-13 18:22:01 +0700

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+reversed ='';
 
+reversed = string.split("").reverse().join("")
+return reversed;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
