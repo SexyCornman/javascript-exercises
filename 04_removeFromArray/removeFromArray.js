4c65ab4f657f9eb4948369776a92a8c25d9676df
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-09-17 00:41:16 +0700

    Finished exercise 4
    Please re-read how rest parameter work if you forget about this lesson

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,37 @@
-const removeFromArray = function() {
+// const removeFromArray = function(array,...args){
+//     const newArray = [];
+  
+//     array.forEach((item) => {
+      
+//       if (!args.includes(item)) {
+//         newArray.push(item);
+//       }
+//     });
+   
+//     return newArray;
+//   };
 
-};
+const removeFromArray = function(array, ...removeItem) {
+     return array.filter(item => !removeItem.includes(item))
 
-// Do not edit below this line
+}
+
+
+
+// this is the code that pass a single test
+// const removeFromArray = function(array,removeItems) {
+//   for(let i=0; i < array.length; i++){
+//     if (array[i] == removeItems){
+//        array.splice(i,1);      
+//     }
+//   }
+// return array;
+//     }
+    
+
+
+
+
+// Do not edit below this line 
+
 module.exports = removeFromArray;
