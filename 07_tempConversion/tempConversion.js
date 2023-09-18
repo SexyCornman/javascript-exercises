241fe6dd881d907ec4d880385316876d21fc5e4c
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-09-18 23:59:39 +0700

    finished exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  let cel = ((temp1-32)*(5/9))*10;
+  return Math.round(cel)/10;;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let fah = (temp2*(9/5)+32)*10;
+  return Math.round(fah)/10;
 };
 
 // Do not edit below this line
