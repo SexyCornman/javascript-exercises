a65b38a9c941ca774e95ca03157e717ffcf98a20
Mainum Suwanjunee <112855044+SexyCornman@users.noreply.github.com>
2023-09-18 22:29:30 +0700

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numMin,numMax){
+    if (numMin < 0 || numMax <0){
+        return "ERROR";
+    }
+    if (!Number.isInteger(numMin) || !Number.isInteger(numMax)){
+        return "ERROR";
+    }
+    if (numMin > numMax){
+         [numMin , numMax] = [numMax, numMin];
+    }
 
-};
+
+    let finalSum =0;
+
+for(let i = numMin; i <= numMax; i++) {
+    finalSum += i;
+}
+return finalSum;
+}
+
 
 // Do not edit below this line
 module.exports = sumAll;
